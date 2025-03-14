2afc2d226cdbde838826e8f2dad4f55cbe5fee45
AntonhyBPV <antonio.parra@campusucc.edu.co>
2025-03-14 09:39:36 -0500

    Simple color palette

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex h-screen w-screen flex-col items-center justify-center bg-cyan-950">
-      <h1 className="text-xl font-bold text-cyan-500">Antonio Parra</h1>
+    <main className="flex h-screen w-screen flex-col items-center justify-center bg-gradient-to-b from-back-primary to-back-secondary">
+      <span className="p-3 mt-5 mb-5 align-middle bg-gradient-to-r from-primary to-secondary rounded-lg">
+        <h1 className="text-xl font-bold text-txt">Antonio Parra</h1>
+      </span>
     </main>
   );
 }
