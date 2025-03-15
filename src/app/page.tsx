e737c10d5c0dcedbff31eaa28834916df98c9a15
AntonhyBPV <antonio.parra@campusucc.edu.co>
2025-03-15 14:48:39 -0500

    added-shadows-to-text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
     <main className="flex h-screen w-screen flex-col items-center justify-center bg-gradient-to-b from-back-primary to-back-secondary">
-      <span className="p-3 mt-5 mb-5 align-middle bg-gradient-to-r from-primary to-secondary rounded-lg">
-        <h1 className="text-xl font-bold text-txt">Antonio Parra</h1>
-      </span>
+      <section className="p-3 mt-5 mb-5 align-middle bg-gradient-to-r from-primary to-secondary rounded-lg shadow-figure">
+        <h1 className="text-xl font-bold text-txt drop-shadow-text">
+          Antonio Parra
+        </h1>
+      </section>
     </main>
   );
 }
