31612d414a1e05c841d78bc4155ec4a53613c552
AntonhyBPV <antonio.parra@campusucc.edu.co>
2025-04-25 15:31:43 -0500

    Made a background Pattern

--- src/app/components/consts/menuItems.ts
+++ src/app/components/consts/menuItems.ts
@@ -14,6 +14,7 @@ import {
   ContactIcon,
   ContactIconHover,
 } from "../icons";
+
 export interface MenuItemData {
   href: string;
   defaultIcon: React.ComponentType<{
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import Header from "./components/Header";
+import BackgroundPattern from "./components/BackgroundPattern";
 
 export default function Home() {
   return (
-    <main className="bg-page flex h-screen w-screen items-center justify-center">
-      <Header />
-      <h1 className="text-3xl font-title font-semibold text-neutral mb-5">
-        Antonio Parra
-      </h1>
-    </main>
+    <>
+      <BackgroundPattern menuWidth={170} className="bg-menu" />
+      <main className="z-10 relative flex h-screen w-screen items-center justify-center">
+        <Header />
+        <h1 className="text-3xl font-title font-semibold text-body">
+          Antonio Parra
+        </h1>
+      </main>
+    </>
   );
 }
